52988a0a2d4bff0ce0450e48f61cbc4f6cfe9719
Naufal <naufal.rayhanwibawa@gmail.com>
2025-12-15 02:44:28 +0000

    relative paths

--- invoker/chained_serverless_invoker/client.py
+++ invoker/chained_serverless_invoker/client.py
@@ -8,8 +8,8 @@ from typing import Any, Callable, Dict, Optional, Tuple
 
 from .config import InvokerConfig
 from .constants import DEFAULT_META_KEY
-from chained_serverless_invoker.invokers import HttpInvoker, PubSubInvoker
-from chained_serverless_invoker.invokers.types import EdgeConfig, InvokerMetadata
+from .invokers import HttpInvoker, PubSubInvoker
+from .invokers.types import EdgeConfig, InvokerMetadata
 
 logger = logging.getLogger(__name__)
 
